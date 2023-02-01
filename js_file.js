fdad158cbdf6f8c2c6b1f06a66ab41b922bdbf7b
Max Burrows <maxajburrows@gmail.com>
2023-02-01 13:41:59 +0100

    Add clear grid button and functionality

--- js_file.js
+++ js_file.js
@@ -1,22 +1,23 @@
 
 
 const createGrid = function (sideLength) {
-    let container = document.querySelector('.container');
-    
+    let container = document.querySelector('.container');   
     totalSquares = sideLength**2;
     for (let i = 1; i <= totalSquares; i++) {
         let square = document.createElement('div');
         square.setAttribute('class', 'square');
         square.addEventListener('mouseover', changeColourOver);
         square.addEventListener('mousedown', changeColourDown);
-        square.ondragstart = () => false;
         container.appendChild(square);
     }
 }
 
 let mouseDown = false;
+//Deal with mousedown events outside the grid.
 document.body.onmouseup = () => mouseDown = false;
 document.body.onmousedown = () => mouseDown = true;
+
+document.body.ondragstart = () => false;
 
 const changeColourDown = function () {
     mouseDown = true;
@@ -28,5 +29,13 @@ const changeColourOver = function () {
     this.setAttribute('class', 'visited');
     }
 }
+
+const clearGrid = function () {
+    visited = document.querySelectorAll('.visited');
+    visited.forEach(square => square.setAttribute('class', 'square'));
+}
+
+let clear = document.querySelector('#reset');
+clear.addEventListener('click', clearGrid);
 
 createGrid(16);  //Create initial grid.
