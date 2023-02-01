5c5e4a11d3bce517e5288cd3648929bec6d3df62
Max Burrows <maxajburrows@gmail.com>
2023-02-01 10:47:35 +0100

    Change sketch functionality from hover to mouse down

--- js_file.js
+++ js_file.js
@@ -1,18 +1,24 @@
 
+let mouseDown = false;
+document.onmousedown = () => mouseDown = true;
+document.onmouseup = () => mouseDown = false;
 
 const createGrid = function (sideLength) {
     let container = document.querySelector('.container');
+    
     totalSquares = sideLength**2;
     for (let i = 1; i <= totalSquares; i++) {
         let square = document.createElement('div');
         square.setAttribute('class', 'square');
-        square.addEventListener('mouseenter', changeColour);
+        square.addEventListener('mouseover', changeColour);
         container.appendChild(square);
     }
 }
 
 const changeColour = function () {
+    if (mouseDown) {
     this.setAttribute('class', 'visited');
+    }
 }
 
 createGrid(16);  //Create initial grid.
